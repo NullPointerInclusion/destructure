e5c0c7d1b61a310cb27eefbf113b54b11c296dfe
NullPointerInclusion <jesseoghenevwegba@outlook.com>
2026-02-12 22:00:23 +0100

    add `DecodeTuple` type

--- src/types.ts
+++ src/types.ts
@@ -1,37 +1,47 @@
-import type { OrArray } from "broadutils/types";
+import type { OrArray } from 'broadutils/types';
 
 type NumericBitLength = 8 | 16 | 32;
-export type NumericType = `${"u" | "i"}${NumericBitLength}`;
+export type NumericType = `${'u' | 'i'}${NumericBitLength}`;
 
 type BigIntBitLength = 64;
-export type BigIntType = `${"u" | "i"}${BigIntBitLength}`;
+export type BigIntType = `${'u' | 'i'}${BigIntBitLength}`;
 
-export type TextType = "char";
+export type TextType = 'char';
 export type FloatType = `f${32 | 64}`;
 export type PrimitiveType = TextType | NumericType | BigIntType | FloatType;
 
-export type SimpleStruct = `${PrimitiveType}${`[${number | ""}]` | ""}`;
-export type ObjectStruct = { [x: string]: Struct }
-export type Struct = CustomStruct<any> | SimpleStruct | ObjectStruct;
+export type SimpleStruct = `${PrimitiveType}${`[${number | ''}]` | ''}`;
+export type ObjectStruct = { [x: string]: Struct };
+export type Struct = CustomStruct<any> | SimpleStruct | ObjectStruct | Struct[];
 
-export type PrimitiveTypeMap = { [K in TextType]: string } & { [K in NumericType | FloatType]: number } & {
+export type PrimitiveTypeMap = { [K in TextType]: string } & {
+  [K in NumericType | FloatType]: number;
+} & {
   [K in BigIntType]: bigint;
 };
 
 type DecodePrimitive<T extends SimpleStruct> = T extends PrimitiveType
   ? PrimitiveTypeMap[T]
-  : T extends `${infer PT extends PrimitiveType}[${number | ""}]`
+  : T extends `${infer PT extends PrimitiveType}[${number | ''}]`
     ? PrimitiveTypeMap[PT][]
     : never;
+type DecodeTuple<T extends Struct[], Collector extends unknown[] = []> = T extends [
+  infer Strct extends Struct,
+  ...infer Rest extends Struct[],
+]
+  ? DecodeTuple<Rest, [...Collector, DecodedStruct<Strct>]>
+  : Collector;
 export type DecodedStruct<Strct extends Struct> = Strct extends SimpleStruct
   ? DecodePrimitive<Strct>
   : Strct extends CustomStruct<infer CS>
     ? CS
-    : Strct extends Struct
-      ? {
-          [K in keyof Strct]: DecodedStruct<Strct[K] extends Struct ? Strct[K] : never>;
-        }
-      : never;
+    : Strct extends Struct[]
+      ? DecodeTuple<Strct>
+      : Strct extends Struct
+        ? {
+            [K in keyof Strct]: DecodedStruct<Strct[K] extends Struct ? Strct[K] : never>;
+          }
+        : never;
 
 export type StructDecodeResult<T> = { value: T; bytesConsumed: number };
 export type SizeOfResult = { value: number; isVariable: boolean };
@@ -49,7 +59,11 @@ export interface CustomStruct<T> {
   size: () => SizeOfResult;
 }
 
-export type PrimtiveEncoder<T> = (value: OrArray<T>, isArray: boolean, arrayLength: number) => number[];
+export type PrimtiveEncoder<T> = (
+  value: OrArray<T>,
+  isArray: boolean,
+  arrayLength: number,
+) => number[];
 export type PrimtiveDecoder<T> = (
   arr: number[],
   offset: number,
